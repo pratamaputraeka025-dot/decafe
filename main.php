8ac69b5fd4157e3cf2d0bbc1ab410b2a64108f51
Tamazy <pratamaputraeka025@gmail.com>
2025-12-14 15:07:23 +0700

    first commit

---
+++ main.php
@@ -0,0 +1,49 @@
+<?php
+//session_start();
+if(empty($_SESSION['username_decafe'])){
+    header('location:login');
+    
+}
+
+    include "proses/connect.php";
+    $query = mysqli_query($conn, "SELECT * FROM tb_user WHERE username = '$_SESSION[username_decafe]'");
+    $hasil = mysqli_fetch_array($query);
+
+    echo $hasil['username'];
+?>
+<!doctype html>
+<html lang="en">
+
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title>DeCafe - Aplikasi Pemesanan Makanan dan MinumanCafe</title>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.8/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-sRIl4kxILFvY47J16cr9ZwB07vP4J8+LH7qKQnuqkuIAvNWLzeN8tE5YBujZqJLB" crossorigin="anonymous">
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.13.1/font/bootstrap-icons.min.css">
+</head>
+
+<body style="height: 3000px">
+    <!-- Header -->
+     <?php include "header.php"; ?>
+
+    <!-- End Header -->
+    <div class="container-lg">
+        <div class="row">
+            <!-- Sidebar -->
+                <?php include "sidebar.php"; ?>
+            <!-- End Sidebar -->
+
+            <!-- Content -->
+             <?php
+                    include $page;           
+            ?>
+            <!-- End Content -->
+        </div>
+
+        <div class="fixed-bottom text-center mb-2">
+            Copyrigth 2025 Tamazy
+        </div>
+        <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.8/dist/js/bootstrap.bundle.min.js" integrity="sha384-FKyoEForCGlyvwx9Hj09JcYn3nv7wiPVlz7YYwJrWVcXK/BmnVDxM+D2scQbITxI" crossorigin="anonymous"></script>
+</body>
+
+</html>
---
+++ report.php
@@ -0,0 +1,15 @@
+<div class="col-lg-9 mt-2">
+                <div class="card">
+                    <div class="card-header">
+                        Report
+                    </div>
+                    <div class="card-body">
+                        <h5 class="card-title">Ini adalah bagian Report</h5>
+                        <p class="card-text">With supporting text below as a natural lead-in to additional content.
+                            Lorem ipsum dolor sit amet consectetur adipisicing elit. Asperiores quis ea nam, dicta sapiente neque excepturi nobis a ullam rerum magnam dolores harum rem cum ratione corporis animi mollitia recusandae!
+                        </p>
+                        <a href="#" class="btn btn-primary">Go somewhere</a>
+                    </div>
+                </div>
+
+            </div>
